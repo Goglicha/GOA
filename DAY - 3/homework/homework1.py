8e5cc0f71e106dcfae2b4002a7c40a8761bec7a7
Goglicha <136063402+Goglicha@users.noreply.github.com>
2023-09-29 23:47:53 +0400

    Add files via upload

---
+++ DAY - 3/homework/homework1.py
@@ -0,0 +1,10 @@
+#მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი და დაპრინტეთ გრძელ წინადადებაში ყველა ეს მონაცემი.
+
+name = input()
+last_name = input()
+age = input()
+print(name + " " + last_name + " " + age)
+
+
+full_name = input(" My name is: ")
+age = input(" i am: ")
