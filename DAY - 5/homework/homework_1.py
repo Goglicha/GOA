9b55dd495335e11a1c2d181ef6f04ac780e27d7e
Goglicha <136063402+Goglicha@users.noreply.github.com>
2023-10-10 14:42:05 +0400

    Add files via upload

---
+++ DAY - 5/homework/homework_1.py
@@ -0,0 +1,7 @@
+# მომხმარებელს კითხეთ თავისი და მამამისის ასაკი, დაუპრინტეთ ყოველ წელს რამდენჯერ უფროსი იქნება მამამისი მასზე
+
+her_age = input("please print your age: ")
+her_fathers_age = input("please print your fathers age: ")
+difference = int(her_fathers_age) / int(her_age)
+print("every year you father will be " + str(difference) + " times older then you")
+
