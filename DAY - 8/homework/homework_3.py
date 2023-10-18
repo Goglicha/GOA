5a39e4c191f8df30abf6646a2eef80914648d42a
Goglicha <136063402+Goglicha@users.noreply.github.com>
2023-10-19 01:30:47 +0400

    Add files via upload

---
+++ DAY - 8/homework/homework_3.py
@@ -0,0 +1,7 @@
+# მომხმარებელს შემოატანინეთ სახელი, და დაპრინტეთ ეს სახელი იმდენჯერ, რამდენი ასოც არის სახელში.
+
+name =input('please enter your name ')
+
+for i in range(len(name)):
+    print(name)
+
---
+++ DAY - 8/homework/homework_5.py
@@ -0,0 +1,16 @@
+# დაპრინტეთ hello world (ოღონდ არა ასე print("hello world") )
+
+#1 
+print("გამარჯობა სამყარო")
+
+#2 
+i = "hello world"
+print(i)
+
+#3
+moral = "If there is a will, there is a way. Every problem has a solution if we just look hard enough and do not give up."
+print(moral[4] + moral[5] + moral[17] + moral[17] + moral[44] + moral[2] + moral[31] + moral[44] + moral[6] + moral[17] + moral[84])
+
+
+
+
